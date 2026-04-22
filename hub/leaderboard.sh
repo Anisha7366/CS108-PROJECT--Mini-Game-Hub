31ee568a5ed11365f291f0824311eb8682499cdb
Alice <cealicevp@gmail.com>
2026-04-22 10:24:04 +0530

    Implemented sorts in leaderboard.sh. It takes command line arguments and sorts accordingly and prints total games played, wins, losses, draws and usernames for all games combined and for each game individually

--- hub/leaderboard.sh
+++ hub/leaderboard.sh
@@ -1,6 +1,77 @@
 #!/bin/bash
 # game[n] = [name, win_num, lose_num, draw_num]
-awk '
+awk -v arg="$1" '
+
+function sort_array(ind, x, n) {
+
+if (x == 4) {
+
+#using infinity as -1 as placeholder, its not being compared anywhere, this is just for clarity of thought
+ for(i=n-1; i>=0; i--) {
+            k=i;
+            if(ind[i,1]==0 && ind[i,2]==0) {max = 0;}
+            else if (ind[i,2]==0) {max = -1;}
+            else {max = ind[i,1]/ind[i,2];}
+
+            for(j=0; j<i; j++) {
+
+                    #if wins and losses are zero; that will be better than win zero and loss one
+                    if (ind[j,1] == 0 && ind[j,2] == 0) {
+                        if(max==0) {if(ind[k,2]>0) {k=j;}}
+                        
+                    }
+
+                    #if both have losses zero we see who has won more
+                    else if(ind[j,2]==0) {if(ind[k,2] == 0 && ind[k,1] < ind[j,1]) {k=j;}
+                                            if(ind[k,2] != 0) {max = -1; k=j;}}
+
+                    #if denominator is not zero, calculated normally
+                    else { if (ind[j, 1]/ind[j, 2] > max && ((ind[k,2] != 0) || (ind[k,2] == 0  && ind[k,1] == 0) )) {max = ind[j, 1]/ind[j,2]; k=j;}
+                           #if ratios are same, higher no of wins is better 
+                           else if (ind[j, 1]/ind[j, 2] == max && ind[j,1] > ind[k,1]) {k=j;} }
+
+            }
+            #print ("switching " ind[i,0] " and " ind[k,0])
+            for (j=0; j<4;j++) {
+               
+                temp = ind[i, j];
+                ind[i, j] = ind[k, j];
+                ind[k, j] = temp; } 
+        }
+
+}
+
+else if (x == 5) {
+
+    for(i=n-1; i>=0; i--) {
+            k=i;
+            max = ind[i,1]+ind[i,2]+ind[i,3];
+            for(j=0; j<i; j++) {
+                    if (ind[j,1]+ind[j,2]+ind[j,3] > max ) {max = ind[j,1]+ind[j,2]+ind[j,3]; k=j;}
+            }
+            for (j=0; j<4;j++) {
+            temp = ind[i, j];
+            ind[i, j] = ind[k, j];
+            ind[k, j] = temp; }
+
+}
+}
+    else { 
+        for(i=n-1; i>=0; i--) {
+            k=i;
+            max = ind[i, x];
+            for(j=0; j<i; j++) {
+                    if (ind[j, x] > max ) {max = ind[j, x]; k=j;}
+            }
+            for (j=0; j<4;j++) {
+                temp = ind[i, j];
+                ind[i, j] = ind[k, j];
+                ind[k, j] = temp; } 
+        }
+}
+
+
+}
 
 function add_row_to_array(user, i) {
 
@@ -92,18 +163,28 @@ return i;
 
 function printing(user, i) {
 
-printf "%-20s %-10s %-10s %-10s\n", "Usernames", "Wins", "Losses", "Draws"
+#print("Function entered")
+
+printf "%-17s %-13s %-10s %-10s %-10s\n", "Usernames", "Total played", "Wins", "Losses", "Draws"
 printf "\n"
-for (j=0; j<i; j++) {
-printf "%-20s %-10s %-10s %-10s\n", user[j,0], user[j,1], user[j,2], user[j,3]
+for (j=i-1; j>=0; j--) {
+printf "%-17s %-13s %-10s %-10s %-10s\n", user[j,0], user[j,1]+user[j,2]+user[j,3], user[j,1], user[j,2], user[j,3]
 
 }
 }
 
-BEGIN {FS=","; OFS = "\t"; i=0; oth=0; four=0; tic=0}
+BEGIN {FS=","; OFS = "\t"; all=0; oth=0; four=0; tic=0
+key = 4; #default because ratio is the fairest way to rank
+if(arg == "wins") {key = 1}
+else if (arg == "losses") {key = 2}
+else if (arg == "draws") {key = 3}
+else if (arg == "ratio") {key = 4}
+else if (arg == "total") {key = 5}
+
+}
 {
 
-i = add_row_to_array(user, i)
+all = add_row_to_array(allgames, all)
 
 if ($6 == "Othello"){
 oth = add_row_to_array(othello, oth) }
@@ -117,9 +198,13 @@ four = add_row_to_array(connect, four)}
 }
 END {
 
+sort_array(allgames, key, all)
+sort_array(tictactoe, key, tic)
+sort_array(othello, key, oth)
+sort_array(connect, key, four)
 
 printf "\nHistory for all games \n"
-printing(user, i)
+printing(allgames, all)
 
 printf "\nHistory for Tic-Tac-Toe \n"
 printing(tictactoe, tic)
@@ -136,3 +221,6 @@ printing(connect, four)
 
 
 ' history.csv
+
+
+#struggle faced: in sorting 0,0 was not being sorted because i had put deno not equal to zero in the normal ratio comparisions and was also relying on normal ratio comparisions to sort the 0,0 case
